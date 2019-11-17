5423b94638173799ccc1b016b225d1c43ce6fb0f
董建新 <824629419@qq.com>
2019-11-17 14:32:23 +0800

    curd 写文档

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('VPJacob 后台管理api')
+    .setDescription('api 接口查看')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+
   await app.listen(3000);
-  console.log('http://localhost:3000')
+  console.log('http://localhost:3000/api-docs')
 }
 bootstrap();
--- server/apps/admin/src/users/users.controller.ts
+++ server/apps/admin/src/users/users.controller.ts
@@ -2,12 +2,14 @@ import { Controller } from '@nestjs/common';
 import { Crud } from 'nestjs-mongoose-crud'
 import { User } from '@libs/db/models/user.model';
 import { InjectModel } from 'nestjs-typegoose';
+import { ApiUseTags } from '@nestjs/swagger';
 
 @Crud(
     { model: User }
 )
 
 @Controller('users')
+@ApiUseTags('用户')
 export class UsersController {
     constructor(@InjectModel(User) private readonly model){
 
